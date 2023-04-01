75b01857039f884cd51edf5b30e2eec3f40714fe
Yaotian-Liu <yaotian_liu@qq.com>
2023-04-01 14:48:36 +0800

    feat: update a lot

--- install.sh
+++ install.sh
@@ -11,24 +11,91 @@ apt upgrade
 apt install -y git tmux python3 pip
 
 
+# Ask if want to copy ssh config & keys from Windows
+echo "Please choose whether to copy ssh config ([y]/n)"
+read -r ssh_config_choice
+ssh_config_choice=${ssh_config_choice:-y} 
+
+if [[ $ssh_config_choice == "y" || $ssh_config_choice == "Y" ]]; then
+    cp -r /mnt/c/Users/24162/.ssh ~/.ssh
+    chmod 600 ~/.ssh/config
+    chmod 600 ~/.ssh/id_rsa
+    chmod 644 ~/.ssh/id_rsa.pub
+    chmod 644 ~/.ssh/known_hosts
+    chmod 644 ~/.ssh/authorized_keys
+
+    echo "Ssh config copied successfully!"
+else
+    echo "Ssh config copy cancelled."
+fi
+
+
+# Ask if want to copy git config from Windows
+echo "Please choose whether to copy git config ([y]/n)"
+read -r git_config_choice
+git_config_choice=${git_config_choice:-y} 
+
+if [[ $git_config_choice == "y" || $git_config_choice == "Y" ]]; then
+    cp /mnt/c/Users/24162/.gitconfig ~/.gitconfig
+    
+    git config --global gpg.program "/mnt/c/Program Files (x86)/gnupg/bin/gpg.exe"
+
+    echo "Git config copied successfully!"
+else
+    echo "Git config copy cancelled."
+fi
+
+
 # install fish
 echo "Installing fish..."
 apt-add-repository ppa:fish-shell/release-3
 apt update
 apt install -y fish
 
-ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
 chsh -s /usr/bin/fish
 
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+cp origin_dotfiles/config.fish.org config.fish
+ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
+
+# Ask about proxy settings
+echo "Please choose whether to set proxy ([y]/n)"
+read -r proxy_choice
+proxy_choice=${proxy_choice:-y} 
+
+if [[ $proxy_choice == "y" || $proxy_choice == "Y" ]]; then
+    # ask about wsl version
+    echo "Please choose whether you are using WSL 1 ([y]/n)"
+    read -r wsl1_choice
+    wsl1_choice=${wsl1_choice:-y} 
+    if [[ $wsl1_choice == "y" || $wsl1_choice == "Y" ]]; then
+        echo "export hostip=\"127.0.0.1\"" >> config.fish
+    else
+        echo "export hostip=\"$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}')\"" >> config.fish
+    fi
+
+    echo "export https_proxy=\"http://$hostip:7890\"" >> config.fish
+    echo "export http_proxy=\"http://$hostip:7890\"" >> config.fish
+else
+    echo "Proxy settings cancelled."
+fi
+
+fish
 
 echo "Fish installed successfully!"
 
 
+# install and configure oh-my-fish
+curl -L https://get.oh-my.fish | fish
+omf install pure
+omf theme pure
+
+echo "Oh-my-fish installed successfully!"
+
 
 # install nodejs
-echo "Please choose whether to install nodejs (y/[n])"
-read nodejs_choice
+echo "Please choose whether to install nodejs ([y]/n)"
+read -r nodejs_choice
+nodejs_choice=${nodejs_choice:-y} 
 
 if [[ $nodejs_choice == "y" || $nodejs_choice == "Y" ]]; then
     echo "Installing nodejs..."
@@ -36,6 +103,20 @@ if [[ $nodejs_choice == "y" || $nodejs_choice == "Y" ]]; then
     apt install -y nodejs
 
     echo "Nodejs installed successfully!"
+
+    # Ask if want to use npm mirror
+    echo "Please choose whether to use npm mirror ([y]/n)"
+    read -r npm_mirror_choice
+    npm_mirror_choice=${npm_mirror_choice:-y} 
+
+    if [[ $npm_mirror_choice == "y" || $npm_mirror_choice == "Y" ]]; then
+        npm config set registry https://registry.npmmirror.com/
+
+        echo "Npm mirror set successfully!"
+    else
+        echo "Npm mirror settings cancelled."
+    fi
+
 else
     echo "Nodejs installation cancelled."
 fi
@@ -45,9 +126,35 @@ fi
 echo "Installing exa..."
 apt install -y exa
 
+echo "alias ls=\"exa --group-directories-first\"" >> config.fish
+echo "alias ll=\"exa -lb --no-user --group-directories-first\"" >> config.fish
+echo "alias la=\"exa -alb --no-user --group-directories-first\"" >> config.fish
+
+echo "Exa installed successfully!"
+
 
 # install zoxide
 echo "Installing zoxide..."
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
 
+echo "zoxide init fish | source" >> config.fish
+echo "alias cd=z" >> config.fish
 
+echo "Zoxide installed successfully!"
+
+
+# install clang-15 and set it to clang
+echo "Installing clang..."
+apt install -y clang-15
+
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-15 100
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-15 100
+
+echo "Clang installed successfully!"
+
+
+# install rust
+echo "Installing rust..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "Rust installed successfully!"
