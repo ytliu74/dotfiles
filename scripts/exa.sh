f5c6b223226e459f555521c6fb4bfaee08ea9328
Yaotian-Liu <yaotian_liu@gmx.com>
2023-04-15 17:38:05 +0800

    feat: exa time style

--- scripts/exa.sh
+++ scripts/exa.sh
@@ -5,7 +5,7 @@ echo "🚀 Installing exa..."
 sudo apt install -y exa
 
 echo "alias ls=\"exa --group-directories-first\"" >> config.fish
-echo "alias ll=\"exa -lb --no-user --group-directories-first\"" >> config.fish
-echo "alias la=\"exa -alb --no-user --group-directories-first\"" >> config.fish
+echo "alias ll=\"exa -lb --time-style long-iso --no-user --group-directories-first\"" >> config.fish
+echo "alias la=\"exa -alb --time-style long-iso --no-user --group-directories-first\"" >> config.fish
 
 echo "Exa installed successfully! 🎉"
