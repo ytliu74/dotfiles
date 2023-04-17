0636dd1aa4fcedd4f406f2b78f247bbbda260575
Yaotian-Liu <yaotian_liu@gmx.com>
2023-04-17 17:01:30 +0800

    feat: Add choice for Alacritty config

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # install alacritty config
-echo "🚀 Installing alacritty config..."
-cp alacritty.yml /mnt/c/Users/$user_name/AppData/Roaming/alacritty/alacritty.yml
-cp argonaut.yaml /mnt/c/Users/$user_name/AppData/Roaming/alacritty/argonaut.yaml
+# Ask if install alacritty config
+echo "Please choose whether to install alacritty config ([y]/n)"
+read -r alacritty_choice
+alacritty_choice=${alacritty_choice:-y} 
+
+if [[ $alacritty_choice == "y" || $alacritty_choice == "Y" ]]; then
+    echo "🚀 Installing alacritty config..."
+    cp alacritty.yml /mnt/c/Users/$user_name/AppData/Roaming/alacritty/alacritty.yml
+    cp argonaut.yaml /mnt/c/Users/$user_name/AppData/Roaming/alacritty/argonaut.yaml
+else
+    echo "Alacritty config cancelled."
+fi
 
 echo "Alacritty config installed successfully! 🎉"
