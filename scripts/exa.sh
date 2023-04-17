09f77ac6e75a99378e6769d5bf933fa3c6889b55
Yaotian-Liu <yaotian_liu@gmx.com>
2023-04-17 13:13:13 +0800

    feat: Add exa icons

--- scripts/exa.sh
+++ scripts/exa.sh
@@ -4,8 +4,8 @@
 echo "🚀 Installing exa..."
 sudo apt install -y exa
 
-echo "alias ls=\"exa --group-directories-first\"" >> config.fish
-echo "alias ll=\"exa -lb --time-style long-iso --no-user --group-directories-first\"" >> config.fish
-echo "alias la=\"exa -alb --time-style long-iso --no-user --group-directories-first\"" >> config.fish
+echo "alias ls=\"exa --icons --group-directories-first\"" >> config.fish
+echo "alias ll=\"exa -lb --icons --time-style long-iso --no-user --group-directories-first\"" >> config.fish
+echo "alias la=\"exa -alb --icons --time-style long-iso --no-user --group-directories-first\"" >> config.fish
 
 echo "Exa installed successfully! 🎉"
