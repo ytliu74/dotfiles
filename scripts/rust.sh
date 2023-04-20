844dd6acf38ca5a3da52c3b65191d91ceee88e1c
Yaotian-Liu <yaotian_liu@gmx.com>
2023-04-20 15:04:27 +0800

    feat: Add rust mirror

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -4,4 +4,6 @@
 echo "🚀 Installing rust..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+ln -s ~/dotfiles/configs/cargo_config.toml ~/.cargo/config.toml
+
 echo "Rust installed successfully! 🎉"
